b2ceb399e7d6c785f4b04304dd66838489743a9e
IsogiE <88090493+IsogiE@users.noreply.github.com>
2025-04-15 13:20:33 +0200

    Teehee

--- Marks.lua
+++ Marks.lua
@@ -1,116 +1,149 @@
-local GlobalAddonName, ExRT = ...
+local RaidMarksModule = {}
+RaidMarksModule.title = "Marks"
 
-local UnitName, GetRaidTargetIndex, SetRaidTargetIcon = UnitName, GetRaidTargetIndex, SetRaidTargetIcon
+local function CreateCustomEditBox(parent, width, height)
+    local edit = CreateFrame("EditBox", nil, parent, "BackdropTemplate")
+    edit:SetSize(width, height)
+    edit:SetAutoFocus(false)
+    edit:SetFont("Fonts\\FRIZQT__.TTF", 14, "") 
+    edit:SetJustifyH("LEFT")
 
-local VMRT = nil
+    edit:SetBackdrop({
+        edgeFile = "Interface\\Buttons\\WHITE8x8",
+        edgeSize = 1,
+    })
+    edit:SetBackdropColor(0, 0, 0, 0) 
+    edit:SetBackdropBorderColor(0.3, 0.3, 0.3, 1) 
 
-local module = ExRT:New("Marks",ExRT.L.Marks)
-local ELib,L = ExRT.lib,ExRT.L
-
-function module.main:ADDON_LOADED()
-	VMRT = _G.VMRT
-	VMRT.Marks = VMRT.Marks or {}
-	VMRT.Marks.list = VMRT.Marks.list or {}
+    edit:SetText("")
+    edit:SetScript("OnEscapePressed", function(self)
+        self:ClearFocus()
+    end)
+    return edit
 end
 
-function module:Enable()
-	module:RegisterTimer()
-	module.Enabled = true
-end
-function module:Disable()
-	module:UnregisterTimer()
-	module.Enabled = nil
+function RaidMarksModule:GetConfigSize()
+    return 800, 600
 end
 
-do
-	local tmr = 0
-	function module:timer(elapsed)
-		tmr = tmr + elapsed
-		if tmr > .5 then
-			tmr = 0
-			for i=1,8 do
-				local name = VMRT.Marks.list[i]
-				if name and name:find("[, ]") then
-					for subname in name:gmatch("[^, ]+") do
-						if UnitName(subname) then
-							if GetRaidTargetIndex(subname)~=i then
-								SetRaidTargetIcon(subname, i)
-							end
-							break
-						end
-					end
-				elseif name and UnitName(name) and GetRaidTargetIndex(name)~=i then
-					SetRaidTargetIcon(name, i)
-				end
-			end
-		end
-	end
+function RaidMarksModule:CreateConfigPanel(parent)
+    if self.configPanel then
+        self.configPanel:SetParent(parent)
+        self.configPanel:ClearAllPoints()
+        self.configPanel:SetAllPoints(parent)
+        self.configPanel:Show()
+        return self.configPanel
+    end
+
+    local configPanel = CreateFrame("Frame", nil, parent)
+    configPanel:SetAllPoints()
+
+    local title = configPanel:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+    title:SetPoint("TOPLEFT", configPanel, "TOPLEFT", 20, 16)
+    title:SetText(self.title)
+
+    local toggleButton = UI:CreateButton(configPanel, "Enable", 140, 30)
+    toggleButton:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -20)
+
+    local clearButton = UI:CreateButton(configPanel, "Clear UI", 140, 30)
+    clearButton:SetPoint("LEFT", toggleButton, "RIGHT", 10, 0)
+
+    toggleButton:SetScript("OnClick", function(self)
+        if not RaidMarksModule.Enabled then
+            RaidMarksModule:Enable()
+            self.text:SetText("Disable")
+        else
+            RaidMarksModule:Disable()
+            self.text:SetText("Enable")
+        end
+    end)
+
+    clearButton:SetScript("OnClick", function()
+        RaidMarksModule:ClearMarks()
+    end)
+
+    self.rows = {}
+    local rowSpacing = 8 
+    local rowStartY  = -10
+    for i = 1, 8 do
+        local icon = configPanel:CreateTexture(nil, "ARTWORK")
+        icon:SetSize(24, 24) 
+        icon:SetTexture("Interface\\TargetingFrame\\UI-RaidTargetingIcon_" .. i)
+        icon:SetPoint("TOPLEFT", toggleButton, "BOTTOMLEFT", 0, rowStartY - (i - 1) * (24 + rowSpacing))
+
+        local editBox = CreateCustomEditBox(configPanel, 300, 24)
+        editBox:SetPoint("LEFT", icon, "RIGHT", 10, 0)
+
+        self.rows[i] = {icon = icon, editBox = editBox}
+    end
+
+    self.configPanel = configPanel
+    return configPanel
 end
 
-function module:SetName(markNum,name)
-	if name == "" then
-		name = nil
-	end
-	VMRT.Marks.list[markNum] = name
+function RaidMarksModule:Enable()
+    self.Enabled = true
+    if not self.timerFrame then
+        self.timerFrame = CreateFrame("Frame")
+        self.timerFrame:SetScript("OnUpdate", function(frame, elapsed)
+            self:OnUpdate(elapsed)
+        end)
+    end
 end
 
-function module:GetName(markNum)
-	return VMRT.Marks.list[markNum]
+function RaidMarksModule:Disable()
+    self.Enabled = false
+    if self.timerFrame then
+        self.timerFrame:SetScript("OnUpdate", nil)
+        self.timerFrame = nil
+    end
 end
 
-function module:ClearNames()
-	for i=1,8 do
-		VMRT.Marks.list[i] = nil
-	end
+function RaidMarksModule:ClearMarks()
+    for i = 1, 8 do
+        local row = self.rows[i]
+        if row and row.editBox then
+            row.editBox:SetText("")
+        end
+    end
 end
 
-
-function module.options:Load()
-	self:CreateTilte()
-
-	local function MarksEditBoxTextChanged(self,isUser)
-		if not isUser then
-			return
-		end
-		local i = self._i
-		local name = self:GetText()
-		if name == "" then
-			name = nil
-		end
-		VMRT.Marks.list[i] = name
-	end
-
-	self.namesEditBox = {}
-	for i=1,8 do
-		self.namesEditBox[i] = ELib:Edit(self):Size(600,20):Point(45,-65-(i-1)*24):Text(VMRT.Marks.list[i]):OnChange(MarksEditBoxTextChanged):Tooltip(L.MarksMultiplyNames)
-		self.namesEditBox[i]._i = i
-		self.namesEditBox[i].icon = ELib:Icon(self.namesEditBox[i],"Interface\\TargetingFrame\\UI-RaidTargetingIcon_"..i,22):Point("RIGHT",self.namesEditBox[i],"LEFT",-5,0)
-	end
-
-	self.showButton = ELib:Button(self,L.Enable):Size(632,20):Point("TOP",0,-35):Tooltip(L.MarksTooltip):OnClick(function (self)
-		if not module.Enabled then
-			self:SetText(L.MarksDisable)
-			module:Enable()
-		else
-			self:SetText(L.Enable)
-			module:Disable()
-		end
-	end)
-	if module.Enabled then
-		self.showButton:SetText(L.MarksDisable)
-	end
-	self.showButton:SetScript("OnShow",function (self)
-		if module.Enabled then
-			self:SetText(L.MarksDisable)
-		else
-			self:SetText(L.Enable)
-		end
-	end)
-	
-	self.clearButton = ELib:Button(self,L.MarksClear):Size(632,20):Point("TOP",0,-265):OnClick(function ()
-		module:ClearNames()
-		for i=1,8 do
-			self.namesEditBox[i]:SetText("")
-		end
-	end)
+function RaidMarksModule:OnUpdate(elapsed)
+    self.tmr = (self.tmr or 0) + elapsed
+    if self.tmr < 0.5 then
+        return
+    end
+    self.tmr = 0 
+    for i = 1, 8 do
+        local row = self.rows[i]
+        if row and row.editBox then
+            local text = row.editBox:GetText()
+            if text and text ~= "" then
+                if text:find("[, ]") then
+                    for name in text:gmatch("([^,%s]+)") do
+                        if UnitName(name) then
+                            if GetRaidTargetIndex(name) ~= i then
+                                SetRaidTargetIcon(name, i)
+                            end
+                            break
+                        end
+                    end
+                elseif UnitName(text) then
+                    if GetRaidTargetIndex(text) ~= i then
+                        SetRaidTargetIcon(text, i)
+                    end
+                end
+            end
+        end
+    end
 end
+
+function RaidMarksModule:OnEnable()
+    self:Enable()
+end
+
+if ACT and ACT.RegisterModule then
+    ACT:RegisterModule(RaidMarksModule)
+end
+
+return RaidMarksModule
