673b3aea919872f89ff29f5757456ba2acdcf417
IsogiE <88090493+IsogiE@users.noreply.github.com>
2025-06-02 19:49:16 +0200

    o7

--- Home.lua
+++ Home.lua
@@ -14,7 +14,7 @@ function HomeModule:CreateConfigPanel(parent)
         self.configPanel:Show()
         return
     end
-    
+
     local configPanel = CreateFrame("Frame", nil, parent)
     configPanel:SetAllPoints()
 
@@ -25,8 +25,23 @@ function HomeModule:CreateConfigPanel(parent)
     local logo = configPanel:CreateTexture(nil, "ARTWORK")
     logo:SetSize(256, 256)
     logo:SetPoint("TOP", configPanel, "TOP", -120, -50)
-    logo:SetTexture("Interface\\AddOns\\ACT_Friends\\media\\logo.tga")
+    logo:SetTexture("Interface\\AddOns\\ACT\\media\\logo.tga")
     logo:SetTexCoord(0.05, 0.95, 0.05, 0.95)
+
+    local brick = configPanel:CreateTexture(nil, "ARTWORK")
+    brick:SetSize(64, 64)
+    brick:SetPoint("TOP", configPanel, "TOP", -120, -450)
+    brick:SetTexture("Interface\\AddOns\\ACT\\media\\roiben.tga")
+    brick:SetTexCoord(0.05, 0.95, 0.05, 0.95)
+
+    local cope = configPanel:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+    cope:SetPoint("BOTTOM", brick, "TOP", 0, 12)
+    cope:SetText("o7 won MDI in our hearts")
+
+    local o7 = configPanel:CreateTexture(nil, "ARTWORK")
+    o7:SetSize(24, 24)
+    o7:SetPoint("RIGHT", cope, "LEFT", -6, 0)
+    o7:SetTexture("Interface\\AddOns\\ACT\\media\\o7.tga")
 
     local info = configPanel:CreateFontString(nil, "OVERLAY", "GameFontNormal")
     info:SetPoint("TOP", logo, "BOTTOM", 0, -10)
